93b0ea7f89ae3c441df681d5ee110c242777b2c4
Anigeth-02 <anigethkallankandy@gmail.com>
2024-12-09 10:07:22 +0530

    My First Commit

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'ebc15d55fe49aadc3b56b045'
+# d62dafb3723b8289e69eff58
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+from market import routes
